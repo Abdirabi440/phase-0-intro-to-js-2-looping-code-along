6399d6943c03349f7cbd2222c5584e3dd5f463d5
Abdirabi440 <osmanabdirabi440@gmail.com>
2024-10-17 14:33:38 +0300

    complete-lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+// 1. writeCards function: 
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+  console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+  // 2. countDown function:
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  countDown(5);
